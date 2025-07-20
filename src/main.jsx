79f025d4a473737bdb5882f9d091843c75da7b70
a-joel <joelvijay319@gmail.com>
2025-07-20 15:57:07 +0530

    Feat: Codes are modified

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import store from "./redux/store.js";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="elite-shopping-dashboard">
       <App />
     </BrowserRouter>
     </Provider>
